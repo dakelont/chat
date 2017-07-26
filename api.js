4512f25048b2854828ffd9595136d9d2623cfee0
dakelont <bk-82-82@bk.ru>
2017-07-26 16:53:57 +0300

    api2

--- api.js
+++ api.js
@@ -3,30 +3,39 @@
 const querystring = require('querystring');
 const http = require('http');
 
-const dataPost = {};
-dataPost.firstName = "Mark";
-dataPost.lastName = "Tomilo";
+var server = http.createServer();
 
-let data = querystring.stringify(dataPost);
+server.on('request', function(req, response) {
+    response.writeHead(200);
+    let getQuery = require('url').parse(req.url, true);
+	
+	const dataPost = {};
+	dataPost.firstName = getQuery.query.firstName;
+	dataPost.lastName = getQuery.query.lastName;
+	
+	let data = querystring.stringify(dataPost);
 
-let options = {
-	hostname: 'netology.tomilomark.ru',
-	port: 80,
-	path: '/api/v1/hash',
-	method: 'POST',
-	headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Content-Length': Buffer.byteLength(data)
-	}
-};
-  let httpreq = http.request(options, function (response) {
-    response.setEncoding('utf8');
-    response.on('data', function (chunk) {
-		chunk = JSON.parse(chunk);
-		dataPost.hash = chunk.hash;
-		console.log(dataPost);
-    });
-  });
+	let options = {
+		hostname: 'netology.tomilomark.ru',
+		port: 80,
+		path: '/api/v1/hash',
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+			'Content-Length': Buffer.byteLength(data)
+		}
+	};
 
-httpreq.write(data);
-httpreq.end();
+	let httpreq = http.request(options, function (response) {
+		response.setEncoding('utf8');
+		response.on('data', function (chunk) {
+			chunk = JSON.parse(chunk);
+			dataPost.hash = chunk.hash;
+			console.log(dataPost);
+		});
+	  });
+	httpreq.write(data);
+	httpreq.end();
+});
+
+server.listen(8081); 
